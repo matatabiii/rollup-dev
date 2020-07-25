fa1b7ee724bc4a18be53f943d72e31cd96b0061f
matatabiii <kawakei.ds@gmail.com>
2020-07-26 05:20:04 +0900

    gulpタスク用テストファイル

---
+++ src/js/foo.js
@@ -0,0 +1,9 @@
+import answer from 'the-answer';
+
+class Foo {
+  constructor () {
+    console.log(answer, 1);
+  }
+}
+
+export default Foo
---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+import Foo from './foo.js'
+
+// async function f () {
+//   const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve('done!'), 0)
+//   })
+
+//   const result = await promise // promise が解決するまで待ちます (*)
+
+//   console.log(result) // "done!"
+// }
+
+new Foo()
